85c37a8581ee139040e38e021137a2e3f63e117d
Sergey Matyukevich <geomatsi@gmail.com>
2020-11-02 23:49:17 +0300

    system: imx8: config for remoteproc/rpmsg tests
    Add imx8 board config for remoteproc/rpmsg fun:
    - Engicam i.Core MX8X: EDIMM SOM  based on NXP i.MX8QXP
    - minimal custom baseboard: UART, USB
    
    Notes
    - use Buildroot configuration for NXP i.MX8QXP-MEK as a starting point
     -- vendor 5.4.24 kernel, u-boot, ATF
    - add package with custom SCU firmware for Engicam i.Core MX8X SOM,
      firmware provided by vendor on github:
      https://github.com/engicam-stable/meta-engicam-mx8/tree/4.19.35/recipes-bsp/imx-sc-firmware/imx-sc-firmware
    - customize kernel configuration
      -- add remoteproc/rpmsg stuff as modules
      -- enable ethernet gadget
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

---
+++ br2-external-system/package/mx8x-icore-sc-firmware/mx8x-icore-sc-firmware.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# mx8x-icore-sc-firmware
+#
+################################################################################
+
+MX8X_ICORE_SC_FIRMWARE_VERSION = 0.1
+MX8X_ICORE_SC_FIRMWARE_SITE = $(BR2_EXTERNAL_SYSTEM_PATH)/board/imx8/icore-mx8x/mx8x-icore-sc-firmware
+MX8X_ICORE_SC_FIRMWARE_SITE_METHOD = local
+MX8X_ICORE_SC_FIRMWARE_LICENSE = Proprietary
+MX8X_ICORE_SC_FIRMWARE_REDISTRIBUTE = NO
+
+define MX8X_ICORE_SC_FIRMWARE_INSTALL_TARGET_CMDS
+	install -m 666 $(@D)/mx8qx-icore-scfw-tcm.bin ${BINARIES_DIR}/
+	
+endef
+
+$(eval $(generic-package))
