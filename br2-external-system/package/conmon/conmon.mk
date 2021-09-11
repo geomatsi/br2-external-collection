9fdc08eaf931dc0c80c57be7e45b338bdca51fa4
Sergey Matyukevich <geomatsi@gmail.com>
2021-09-12 00:36:14 +0300

    conmon: new package
    Add conmon - an OCI container runtime monitor.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

---
+++ br2-external-system/package/conmon/conmon.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# conmon
+#
+################################################################################
+
+CONMON_VERSION = v2.0.29
+CONMON_SITE = $(call github,containers,conmon,$(CONMON_VERSION))
+
+CONMON_LICENSE = Apache-2.0
+CONMON_LICENSE_FILES = LICENSE
+
+define CONMON_BUILD_CMDS
+	cd $(@D) && $(TARGET_MAKE_ENV) $(MAKE) \
+		CC="$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS)"
+endef
+
+define CONMON_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/bin/conmon $(TARGET_DIR)/usr/bin/conmon
+endef
+
+$(eval $(generic-package))
