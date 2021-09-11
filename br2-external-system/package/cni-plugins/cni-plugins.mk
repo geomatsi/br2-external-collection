38448f54df6d6fa141b241b59e1763c5893a32d3
Sergey Matyukevich <geomatsi@gmail.com>
2021-09-12 00:33:35 +0300

    cni-plugins: new package
    Set of CNI network plugins, maintained by the containernetworking team.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

---
+++ br2-external-system/package/cni-plugins/cni-plugins.mk
@@ -0,0 +1,39 @@
+################################################################################
+#
+# cni-plugins
+#
+################################################################################
+
+CNI_PLUGINS_VERSION = v1.0.1
+CNI_PLUGINS_SITE = $(call github,containernetworking,plugins,$(CNI_PLUGINS_VERSION))
+
+CNI_PLUGINS_LICENSE = Apache-2.0
+CNI_PLUGINS_LICENSE_FILES = LICENSE
+
+CNI_PLUGINS_BUILD_TARGETS = \
+	plugins/meta/bandwidth 		\
+	plugins/meta/firewall 		\
+	plugins/meta/portmap		\
+	plugins/meta/sbr		\
+	plugins/meta/tuning		\
+	plugins/meta/vrf		\
+	plugins/ipam/dhcp		\
+	plugins/ipam/host-local		\
+	plugins/ipam/static		\
+	plugins/main/bridge		\
+	plugins/main/host-device	\
+	plugins/main/ipvlan		\
+	plugins/main/loopback		\
+	plugins/main/macvlan		\
+	plugins/main/ptp		\
+	plugins/main/vlan
+
+define CNI_PLUGINS_INSTALL_TARGET_CMDS
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/cni/net.d
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/lib/cni
+
+	cp -r $(@D)/bin $(TARGET_DIR)/usr/lib/cni/
+	cp -r $(BR2_EXTERNAL_SYSTEM_PATH)/package/cni-plugins/conf/* $(TARGET_DIR)/etc/cni/net.d/
+endef
+
+$(eval $(golang-package))
