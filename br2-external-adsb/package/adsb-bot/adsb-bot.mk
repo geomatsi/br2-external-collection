98faf983c8495716c374626162fd092b4750a9d0
Sergey Matyukevich <geomatsi@gmail.com>
2022-02-15 00:36:32 +0300

    adsb-bot: update golang package
    Update adsb-bot package location: use release from github.
    Update golang-package structure according to golang
    infrastructure available in Buildroot v2022.02.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- br2-external-adsb/package/adsb-bot/adsb-bot.mk
+++ br2-external-adsb/package/adsb-bot/adsb-bot.mk
@@ -4,24 +4,18 @@
 #
 ################################################################################
 
-ADSB_BOT_VERSION = 3a924b237ecd3219a277520a85d052194cfb2724
-ADSB_BOT_SITE = $(call github,geomatsi,go-tests,$(ADSB_BOT_VERSION))
+ADSB_BOT_VERSION = v0.1
+ADSB_BOT_SOURCE = adsb-bot-$(ADSB_BOT_VERSION).tar.gz
+ADSB_BOT_SITE = https://github.com/geomatsi/go-tests/releases/download/go-tests--adsb-bot-$(ADSB_BOT_VERSION)
 
-ADSB_BOT_SRC_SUBDIR = github.com/geomatsi/adsb-bot
-ADSB_BOT_GO_ENV = GO111MODULE=on
-
-define ADSB_BOT_TRIM_SOURCE
-	rm -rf $(@D)/tests $(@D)/tour
-	mv $(@D)/adsb-bot/* $(@D)
-	rm -rf $(@D)/adsb-bot
-endef
+ADSB_BOT_GO_ENV = GOPROXY=proxy.golang.org
+ADSB_BOT_GOMOD = adsb-bot
 
 define ADSB_BOT_INSTALL_CONFIG_FILES
 	$(INSTALL) -d -m 755 $(TARGET_DIR)/etc/adsb
 	$(INSTALL) -D -m 0644 $(@D)/data/bot.json $(TARGET_DIR)/etc/adsb/adsb_bot.conf
 endef
 
-ADSB_BOT_POST_EXTRACT_HOOKS += ADSB_BOT_TRIM_SOURCE
 ADSB_BOT_POST_INSTALL_TARGET_HOOKS += ADSB_BOT_INSTALL_CONFIG_FILES
 
 define ADSB_BOT_INSTALL_INIT_SYSTEMD
