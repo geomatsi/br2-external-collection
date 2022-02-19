0458a019aa9ac3e73ae1da08ad2c3cb051da9213
Sergey Matyukevich <geomatsi@gmail.com>
2022-02-19 17:10:52 +0300

    bluetooth: update example
    Update bluetooth layer to work with Buildroot v2022.02. For this
    purpose update Linux kernel version and replace outdated options
    by their up-to-date alternatives.
    
    Simplify example: keep only bluetooth functions, remove connman
    gadget example.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- br2-external-bluetooth/board/raspberrypi0w/post-build.sh
+++ br2-external-bluetooth/board/raspberrypi0w/post-build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 BOARD_DIR="$(dirname $0)"
 
+install -m 0644 -D $BOARD_DIR/cmdline.txt $BINARIES_DIR/rpi-firmware/cmdline.txt
 install -m 0644 -D $BOARD_DIR/config.txt $BINARIES_DIR/rpi-firmware/config.txt
