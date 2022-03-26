3bac712de7b9cf84fea09d02d7f09f316d1048d1
Sergey Matyukevich <geomatsi@gmail.com>
2022-03-26 23:59:42 +0300

    cni: remove redundant vendoring
    Golang package dependencies are now downloaded during fetch stage.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- br2-external-system/package/cni/cni.mk
+++ br2-external-system/package/cni/cni.mk
@@ -9,14 +9,6 @@ CNI_SITE = $(call github,containernetworking,cni,$(CNI_VERSION))
 CNI_LICENSE = Apache-2.0
 CNI_LICENSE_FILES = LICENSE
 
-CNI_GO_ENV = GOPROXY=proxy.golang.org
-
-define CNI_DOWNLOAD_VENDOR_PKGS
-	cd $(@D); $(HOST_GO_TARGET_ENV) $(CNI_GO_ENV) $(GO_BIN) mod vendor
-endef
-
-CNI_PRE_BUILD_HOOKS += CNI_DOWNLOAD_VENDOR_PKGS
-
 CNI_BUILD_TARGETS = cnitool
 CNI_INSTALL_BINS = cnitool
 
